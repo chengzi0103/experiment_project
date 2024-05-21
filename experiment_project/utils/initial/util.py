904efa1c8e9f8b3ece60604b4e13bea00b12482b
cheng.chen <chencheng0103@outlook.com>
2024-05-21 17:29:37 +0800

    feat: 更新embdding的内容

---
+++ experiment_project/utils/initial/util.py
@@ -0,0 +1,5 @@
+import os
+def init_sys_env(is_proxy:bool=True,proxy_url:str='http://192.168.0.75:10809',):
+    if is_proxy:
+        os.environ['http_proxy'] = proxy_url
+        os.environ['https_proxy'] = proxy_url
